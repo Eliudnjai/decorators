64f825379e141e2851e4e5fa1a8e85a17ff7ad7b
Eliudnjai <Eliudnjai@gmail.com>
2018-01-06 21:09:37 -0500

    first_commit

---
+++ decorators.py
@@ -0,0 +1,37 @@
+import functools
+
+def my_decorators(func):
+    @functools.wraps(func)
+    def My_function():
+        print("HI")
+        func()
+    return My_function
+
+@my_decorators
+def new_function():
+    print("world")
+
+#new_function()
+
+
+
+#decorators with argu
+def decorator_with_number(number):
+    def first_decorators(func):
+        @functools.wraps(func)
+        def second_decorator(*args,**kwargs):
+            print("HI")
+            if number == 58:
+                print("this works")
+            else:
+                func()
+            print("end")
+        return second_decorator
+    return first_decorators
+
+
+@decorator_with_number(57)
+def my_function_new():
+    print("client")
+
+my_function_new()
